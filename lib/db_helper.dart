0911f77ddc32b387cdc971a478a04f55a1628445
muhammadXainAhmad <206599128+muhammadXainAhmad@users.noreply.github.com>
2025-04-14 01:23:31 +0500

    Created addItem function

--- lib/db_helper.dart
+++ lib/db_helper.dart
@@ -5,6 +5,10 @@ import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DBHelper {
+  final String TABLE_TODO = "todo";
+  final String COLUMN_TODO_ID = "id";
+  final String COLUMN_TODO_TEXT = "text";
+  final String COLUMN_TODO_ISDONE = "is_done";
   DBHelper._();
 
   static final DBHelper getInstance = DBHelper._();
@@ -24,5 +28,19 @@ class DBHelper {
     Directory appDir = await getApplicationDocumentsDirectory();
     String dbPath = join(appDir.path, "todoDB.db");
     return await openDatabase(dbPath, onCreate: (db, version) {}, version: 1);
+  }
+
+  Future<bool> addItem({
+    required String mId,
+    required String mText,
+    required bool mIsDone,
+  }) async {
+    var db = await getDB();
+    int rowsEffected = await db.insert(TABLE_TODO, {
+      COLUMN_TODO_ID: mId,
+      COLUMN_TODO_TEXT: mText,
+      COLUMN_TODO_ISDONE: mIsDone,
+    });
+    return rowsEffected > 0;
   }
 }
